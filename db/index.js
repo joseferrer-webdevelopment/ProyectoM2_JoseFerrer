9b76cadece2074531bc8dec4300b72002bbd9536
Jose Peñuela <269219523+joseferrer-webdevelopment@users.noreply.github.com>
2026-07-12 10:38:53 -0500

    feat: support DATABASE_URL for production deployment

--- db/index.js
+++ db/index.js
@@ -1,12 +1,22 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
+const dbConnectionLocal = {
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-});
+}
+
+const dbConnectionProduction = {
+  connectionString: process.env.DATABASE_URL
+}
+
+const pool = new Pool( process.env.DATABASE_URL ? dbConnectionProduction : dbConnectionLocal)
 
 module.exports = pool;
+
+
+
+
